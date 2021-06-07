8b0d1a84d23bf8f9323e60c23f72ecec8b3344bd
Catherinewycherley <catherinewycherley@gmail.com>
2021-06-07 19:06:49 +0100

    created test2, updated main & test

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
 print ("hello monday")
 print (5+15)
 print ("hello 9678")
-
+print ("56987456")
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print ("Bank Holiday Monday")
 print (5+6+7+8+9)
-print ("hello 123")
+print ("Monday")
