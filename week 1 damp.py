d55315df8a0069323d031afbeeef38cd80918c8f
Catherinewycherley <catherinewycherley@gmail.com>
2021-06-07 22:09:21 +0100

    added week 1 damp updated 7th June 22.09

--- week 1 damp.py
+++ week 1 damp.py
@@ -20,3 +20,25 @@ pe_ratio = 44
 print(type(pe_ratio))
 print (1==2)
 print (1==1)
+print (type(1==1))
+x=5
+print (x*5)
+y='stock'
+print(y*3)
+company_1="Apple"
+print (company_1)
+year_1=2017
+print(year_1)
+revenue_4=22698.36
+print(revenue_1)
+print(revenue_4)
+print(type(company_1))
+print(type(year_1))
+print(type(revenue_4))
+test_company='Apple'
+print(company_1 == test_company)
+print(revenue_1 > revenue_2)
+year_1_str=str(year_1)
+revenue_4_str=str(revenue_4)
+sentence = 'The revenue of'  +    company_1 + ' in' + ' 2017' + ' was' +'  229.23' + ' billion.'
+print(sentence)
