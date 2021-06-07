83b9acafb8913a5c004ce59d95b054820ceaf115
Catherinewycherley <catherinewycherley@gmail.com>
2021-06-07 20:09:42 +0100

    added week 1 damp updated 20.09

--- week 1 damp.py
+++ week 1 damp.py
@@ -7,3 +7,12 @@ print ("Pe_ratio")
 print (10+2)
 print (10*2)
 print (10-7)
+
+revenue_1=50
+revenue_2=60
+revenue_3=70
+total = revenue_1+revenue_2+revenue_3
+print (total)
+print("total")
+average= total/3
+print (average)
