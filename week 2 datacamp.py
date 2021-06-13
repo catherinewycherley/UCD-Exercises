db7131753b2e6ed3992a38404b69904bf5c38d1e
Catherinewycherley <catherinewycherley@gmail.com>
2021-06-13 15:04:33 +0100

    updated week 2 damp.py 13th June 15.04

--- week 2 datacamp.py
+++ week 2 datacamp.py
@@ -2,3 +2,8 @@ cusip_lookup = {}
 cusip_lookup['38259P706'] = 'GOOG'
 cusip_lookup['037833100'] = 'AAPL'
 print(cusip_lookup['037833100'])
+cash=19.11
+non_cash=19
+print(cash==non_cash)
+cash=non_cash
+print(cash==non_cash)
