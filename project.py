f6b3acbc1ba7b42647e1f47b0ac03f8fddc3b300
Catherinewycherley <catherinewycherley@gmail.com>
2021-06-29 22:37:53 +0100

    project.py company overview added 29th June 2021

--- project.py
+++ project.py
@@ -4,3 +4,9 @@ print(aapl)
 print(aapl.describe())
 print(aapl.dtypes)
 print(aapl.head())
+import requests
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=aapl&apikey=CJW6S4N6QQGYGWPZ'
+r = requests.get(url)
+company_overview = r.json()
+
+print(company_overview)
