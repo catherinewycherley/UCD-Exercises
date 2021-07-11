b9a95169efe82a8e32627df013482c67cc9d40bd
Catherinewycherley <catherinewycherley@gmail.com>
2021-07-11 18:10:06 +0100

    RubricCW.py company (Apple) overview retrieved from online API

--- RubricCW.py
+++ RubricCW.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 plt.style.use('dark_background')
 
 
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=aapl&apikey=CJW6S4N6QQGYGWPZ'
+r = requests.get(url)
+company_overview = r.json()
+
+print(company_overview)
+
