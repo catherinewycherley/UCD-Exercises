94c52fb434656ad52a113e8ec20e196d4af7adf6
Catherinewycherley <catherinewycherley@gmail.com>
2021-07-13 22:36:12 +0100

    RubricCW.py msft df for loop to print column names. For loop to add in column share price change to iterate over each row and break loop.  print head (first 5 rows)

--- RubricCW.py
+++ RubricCW.py
@@ -94,3 +94,14 @@ msft["Date"] = pd.to_datetime(msft["Date"])
 print(msft.info())
 
 msft.set_index(keys='Date', inplace=True)
+
+
+
+
+for i in msft:
+    print (i)
+
+    for ind, row in msft.iterrows():
+        msft.loc[ind, [" Share Price Change"]] = row['Close'] - row['Open']
+        print(msft.head())
+        break
